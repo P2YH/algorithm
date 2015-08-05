71adc828ba905f35c61dd3efd829bab15e663b13
P2YH <biggoldroc@gmail.com>
2015-08-05 16:40:44 +0800

    增加插入排序
    Time Complexity: O(n*n)
    Auxiliary Space: O(1)
    Boundary Cases: Insertion sort takes maximum time to sort if elements are sorted in reverse order. And it takes minimum time (Order of n) when elements are already sorted.
    Algorithmic Paradigm: Incremental Approach
    Sorting In Place: Yes
    Stable: Yes
    Online: Yes
    Uses: Insertion sort is uses when number of elements is small. It can also be useful when input array is almost sorted, only few elements are misplaced in complete big array.
    
    时间复杂度：O(n*n)
    空间复杂度: O(1)
    边界值：当待排序的元素是逆序的时候插入排序花费时间最大，当待排序的元素已经排好序的时候插入排序花费时间最小。
    使用：当待排序的元素比较少，或者差不多已经排好序，只有少数的元素不在正确的位置上的大数组。

--- sort.c
+++ sort.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
 
+//打印数组
+void printArray(int nArray[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d," , nArray[i]);
+	}
+	printf("\n");
+}
+
+//插入排序
+void InserSort(int nArray[], int n)
+{
+	int key = 0;
+	for (int i = 1; i < n; i++)
+	{
+		key = nArray[i];
+		int j = i - 1;
+		while (j >= 0 && nArray[j] > key)//遍历key之前已经排好序的数，如果这个数大于key值那么进入后移操作
+		{
+			nArray[j + 1] = nArray[j];//大的数后移一位
+			j--;//游标前移
+		}
+		nArray[j + 1] = key;//插入到顺序的位置
+	}
+}
+
+int main()
+{
+	int nArray[] = {10,3,5,2,1,4};
+	int nSize = sizeof(nArray)/sizeof(nArray[0]);
+	printArray(nArray, nSize);
+	InserSort(nArray, nSize);
+	printArray(nArray, nSize);
+	getchar();
+	return 0;
+}
