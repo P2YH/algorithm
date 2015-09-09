3b636326792428179ffcc85c418c58a597c7b92b
P2YH <biggoldroc@gmail.com>
2015-09-09 13:36:01 +0800

    注释

--- Leecode/myAtoi.cpp
+++ Leecode/myAtoi.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int myAtoi(string str) {
+		//空字符串判定
         if (str.empty())
     	{
     		return 0;
@@ -9,14 +10,17 @@ public:
      	long long Res = 0;
     	int sign = 1;
     	int i = 0;
+		//排除空字符
     	while (str[i] == ' ')
     	{
     		i++;
     	}
+		//判断正负号
     	if ( str[i] == '-')
     	{
     		sign = -1;
     		i++;
+			//正负号后紧跟数字判断
     		if(str[i] < '0' || str[i] > '9')
         	{
         		return 0;
@@ -26,12 +30,15 @@ public:
     	{
     	    i++;
     	}
+		//第一个非空字符必须是 + - 数字
+		//这里也自然就排除了无数字字符的字符串
     	if(str[i] < '0' || str[i] > '9')
     	{
     	    return 0;
     	}
     	for(; i <= nSize; i++)
     	{
+			//截取到第一个不为数字字符的位置
     	   	if(str[i] < '0' || str[i] > '9')
         	{
         	   break;
@@ -39,6 +46,7 @@ public:
     	   	if(str[i] >= '0' && str[i] <= '9')
         	{
         		Res = Res * 10 + str[i] - '0';
+				//整型最大最小值判断
         		if ( Res > INT_MAX)
         		{
         			Res = sign < 0 ? INT_MIN : INT_MAX;
