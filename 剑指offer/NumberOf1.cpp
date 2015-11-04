63bfbcf281267faa2dcf05f62b4ae744e3ab73cd
P2YH <biggoldroc@gmail.com>
2015-11-04 13:20:07 +0800

    2进制整数含1的个数

---
+++ 剑指offer/NumberOf1.cpp
@@ -0,0 +1,21 @@
+//32位操作系统中循环32次
+int  NumberOf1(int n) {
+ int iFlag = 1;
+ int iCount = 0;
+ while(iFlag){
+	 if(n & iFlag){
+		 iCount++;
+	 }
+	 iFlag = iFlag<<1;
+ }
+ return iCount;
+}
+//有多少个1就循环几次
+int NumberOf1(int n){
+	int iCount = 0;
+	while(n){
+		if(n & (n - 1)){
+			++iCount;
+		}
+	}
+}
